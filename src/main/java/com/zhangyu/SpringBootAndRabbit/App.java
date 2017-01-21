698cf6792120bbc56ff755eaafd25e3685d29294
张家小公子 <852611546@qq.com>
2017-01-21 16:23:59 +0800

    本地上传springboot及rabbitMQ的测试项目

---
+++ src/main/java/com/zhangyu/SpringBootAndRabbit/App.java
@@ -0,0 +1,34 @@
+package com.zhangyu.SpringBootAndRabbit;
+
+import com.zhangyu.SpringBootAndRabbit.controller.SendController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+@EnableAutoConfiguration
+public class App
+{
+    @RequestMapping("/h")
+    public String home() {
+        return "Hello";
+    }
+
+    @RequestMapping("/w")
+    public String word() {
+        return "World";
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World ! App!" );
+//        SpringApplication.run(App.class, args);
+        SpringApplication.run(SendController.class);
+    }
+
+}
