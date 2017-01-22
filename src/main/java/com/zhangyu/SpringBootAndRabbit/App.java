e3e987923367769ad66e93b407d4563080340f43
张家小公子 <852611546@qq.com>
2017-01-22 14:39:52 +0800

    1、启动文件中添加消费者

--- src/main/java/com/zhangyu/SpringBootAndRabbit/App.java
+++ src/main/java/com/zhangyu/SpringBootAndRabbit/App.java
@@ -1,8 +1,10 @@
 package com.zhangyu.SpringBootAndRabbit;
 
+import com.zhangyu.SpringBootAndRabbit.controller.RecvController;
 import com.zhangyu.SpringBootAndRabbit.controller.SendController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class App
 {
+    public static ApplicationContext context;
+
+    public static <T> T getBean(Class<T> c) {
+        return context.getBean(c);
+    }
+
     @RequestMapping("/h")
     public String home() {
         return "Hello";
@@ -24,11 +32,13 @@ public class App
         return "World";
     }
 
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World ! App!" );
 //        SpringApplication.run(App.class, args);
         SpringApplication.run(SendController.class);
+//        SpringApplication.run(RecvController.class);
     }
 
 }
